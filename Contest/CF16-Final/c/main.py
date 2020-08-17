b44ec780d9e9bfe77e84e88548678913c00510ba
Orangeat <nsorangepv@gmail.com>
2020-08-17 16:18:41 +0900

    Solve ABC126-E etc.

--- Contest/CF16-Final/c/main.py
+++ Contest/CF16-Final/c/main.py
@@ -1,1 +1,45 @@
 #!/usr/bin/env python3
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+(n, m), *LL = [[*map(int,o.split())] for o in open(0)]
+UF = UnionFind(n + m)
+for e, (k, *L) in enumerate(LL):
+    for l in L:
+        UF.union(e, l + n - 1)
+print("YES" if len([i for i, x in enumerate(UF.parents) if x < 0 and i < n]) == 1 else "NO")
