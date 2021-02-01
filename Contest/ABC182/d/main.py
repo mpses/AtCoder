113692c5d815416972456a4097c6f7f3692e4c6c
Orangeat <nsorangepv@gmail.com>
2021-02-02 00:47:09 +0900

    ABC182

--- Contest/ABC182/d/main.py
+++ Contest/ABC182/d/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+n, *a = map(int, open(0).read().split())
+from itertools import*
+*S, = accumulate(a)
+*M, = accumulate(S, max)
+Z = ans = 0
+for s, m in zip(S, M):
+    ans = max(ans, Z + m)
+    Z += s
+print(ans)
--- Contest/ABC183/c/main.py
+++ Contest/ABC183/c/main.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+(n, k), *t = [[*map(int, o.split())] for o in open(0)]
+from itertools import*
+ans = 0
+for s in permutations(range(1, n)):
+    s = [0, *s, 0]
+    ans += sum(t[i][j] for i, j in zip(s, s[1:])) == k
+print(ans)
